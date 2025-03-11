9a78dc5fe86c72bc3340872d0a93daab171ffb80
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-03-11 21:30:35 +0530

    refactor: remove Signup and Signin components; clean up layout and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="p-4 border-b">
-          BLOG
-        </div>
         {children}
       </body>
     </html>
